608b747d10eb95eff1b99da8ab8e75de8844b348
Caio Cabral <ocaiocabral@gmail.com>
2024-01-26 17:19:00 -0500

    feat: enhancing token validation to avoid errors

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
 export default async function InitialPage() {
-	const token = cookies().get("token")!.value;
-	token !== "" && token !== undefined ? redirect("/home") : redirect("/login");
+	const token = cookies().get("token");
+	if (token) {
+		redirect("/home");
+	} else {
+		redirect("/login");
+	}
 }
