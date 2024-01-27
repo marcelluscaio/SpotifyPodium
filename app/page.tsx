3913f22310727253818fa3f7de8ea04c21e12ade
Caio Cabral <ocaiocabral@gmail.com>
2024-01-27 18:03:15 -0500

    refactor changing login logic to avoid errors

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { cookies } from "next/headers";
 
 export default async function InitialPage() {
 	const token = cookies().get("token");
-	if (token) {
-		redirect("/home");
-	} else {
+	if (!token || token.value === "") {
 		redirect("/login");
+	} else {
+		redirect("/home");
 	}
 }
